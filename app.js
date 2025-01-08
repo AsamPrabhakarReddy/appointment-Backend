d0bb0ae4d211575edbbf56869e6060d1f378a110
ASAM PRABHAKAR REDDY <asamprabhakarbala@gmail.com>
2025-01-08 11:38:46 +0530

    added some routes

--- app.js
+++ app.js
@@ -15,13 +15,13 @@ const app = express();
 // Middlewares
 app.use(express.json());
 
-const corsOptions = {
-  origin: 'https://mannam-syndeo-ui.vercel.app/', // Allow specific origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
-  credentials: true // Include credentials if needed
-};
+// const corsOptions = {
+//   origin: 'https://mannam-syndeo-ui.vercel.app/', // Allow specific origin
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
+//   credentials: true // Include credentials if needed
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json());
 
 // Use the slotRoutes
